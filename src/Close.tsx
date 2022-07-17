27f64f22719fd637849f38d06651fef870f34166
between0n1 <sunhyeok.jung22@myhunter.cuny.edu>
2022-07-16 20:30:57 -0400

    ASD

--- src/Close.tsx
+++ src/Close.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 
 type CloseProps = {
   onPress: () => void;
@@ -9,9 +9,25 @@ type CloseProps = {
 
 export const Close = ({ onPress, rootStyle, iconStyle }: CloseProps) => {
   return (
-    <TouchableOpacity activeOpacity={0.5} onPress={() => onPress()} style={[CloseStyles.closeButton, rootStyle]}>
-      <View style={[CloseStyles.iconLine, iconStyle, { transform: [{ rotateZ: '45deg' }] }]} />
-      <View style={[CloseStyles.iconLine, iconStyle, { transform: [{ rotateZ: '135deg' }] }]} />
+    <TouchableOpacity
+      activeOpacity={0.5}
+      onPress={() => onPress()}
+      style={[CloseStyles.closeButton, rootStyle]}
+    >
+      <View
+        style={[
+          CloseStyles.iconLine,
+          iconStyle,
+          { transform: [{ rotateZ: "220deg" }] },
+        ]}
+      />
+      <View
+        style={[
+          CloseStyles.iconLine,
+          iconStyle,
+          { transform: [{ rotateZ: "135deg" }] },
+        ]}
+      />
     </TouchableOpacity>
   );
 };
@@ -21,20 +37,20 @@ const CloseStyles = StyleSheet.create({
     width: 30,
     height: 30,
     borderRadius: 15,
-    position: 'absolute',
+    position: "absolute",
     right: 15,
     top: 15,
-    backgroundColor: '#e2e2e2',
+    backgroundColor: "#e2e2e2",
     zIndex: 3,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
   iconLine: {
-    position: 'absolute',
+    position: "absolute",
     width: 18,
     height: 2,
     borderRadius: 2,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
 });
